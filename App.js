5ce40601c7892547aca216b5590dd2916b90b30b
hitaesh77 <hitaesh777@gmail.com>
2025-03-13 01:29:32 -0400

    Tasks Screen

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Tasks Header */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+        </View>
+
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fffcf2',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
   },
 });
